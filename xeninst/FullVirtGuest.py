8ce15a5b4cabad660a2fea85d5024c473ad7ca54
Jeremy Katz <katzj@redhat.com>
2006-08-09 12:05:25 -0400

    first pass at full virt installs.  doesn't currently work due to libvirt not fully understanding cd devices for hvm guests

--- xeninst/FullVirtGuest.py
+++ xeninst/FullVirtGuest.py
@@ -12,13 +12,23 @@
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
 
-import os
+import os,stat,time,sys
+import string
+
+import libvirt
 
 import XenGuest
+
+if os.uname()[4] in ("x86_64"):
+    qemu = "/usr/lib64/xen/bin/qemu-dm"
+else:
+    qemu = "/usr/lib/xen/bin/qemu-dm"
 
 class FullVirtGuest(XenGuest.XenGuest):
     def __init__(self):
         XenGuest.XenGuest.__init__(self)
+        self._cdrom = None
+        self.disknode = "ioemu:hd"
 
     def get_cdrom(self):
         return self._cdrom
@@ -29,26 +39,92 @@ class FullVirtGuest(XenGuest.XenGuest):
         self._cdrom = val
     cdrom = property(get_cdrom, set_cdrom)
 
+    def _get_disk_xml(self):
+        # ugh, this is disgusting, but the HVM disk stuff isn't nice :/
+        x = XenGuest.XenGuest._get_disk_xml(self)
+        lines = x.split("\n")
+        if len(lines) > 3:
+            lines = lines[:3]
+        if stat.S_ISBLK(os.stat(self.cdrom)[stat.ST_MODE]):
+            t = "block"
+        else:
+            t = "file"
+        lines.append("<disk type='%(disktype)s'><source file='%(disk)s'/><target dev='hdd:cdrom'/></disk>\n" %{"disktype": t, "disk": self.cdrom})
+        return string.join(lines, "")
+
+    def _get_config_xml(self):
+        # FIXME: hard-codes that we're booting from CD as hdd
+        return """<domain type='xen'>
+  <name>%(name)s</name>
+  <os>
+    <type>hvm</type>
+    <loader>/usr/lib/xen/boot/hvmloader</loader>
+    <boot dev='hdd'/>
+  </os>
+  <memory>%(ramkb)s</memory>
+  <vcpu>%(vcpus)d</vcpu>
+  <uuid>%(uuid)s</uuid>
+  <on_reboot>destroy</on_reboot>
+  <on_poweroff>destroy</on_poweroff>
+  <on_crash>destroy</on_crash>
+  <devices>
+    <emulator>%(qemu)s</emulator>
+    %(disks)s
+    %(networks)s
+    <graphics type='vnc'/>
+  </devices>
+</domain>
+""" % { "qemu": qemu, "name": self.name, "vcpus": self.vcpus, "uuid": self.uuid, "ramkb": self.memory * 1024, "disks": self._get_disk_xml(), "networks": self._get_network_xml() }
+
+    def _get_config_xen(self):
+        return """# Automatically generated xen config file
+name = "%(name)s"
+builder = "hvm"
+memory = "%(ram)s"
+%(disks)s
+%(networks)s
+uuid = "%(uuid)s"
+device_model = "%(qemu)s"
+kernel = "/usr/lib/xen/boot/hvmloader"
+vnc = 1
+serial = "pty" # enable serial console
+on_reboot   = 'restart'
+on_crash    = 'restart'
+""" % { "name": self.name, "ram": self.memory, "disks": self._get_disk_xen(), "networks": self._get_network_xen(), "uuid": self.uuid, "qemu": qemu }
+
     
-    # install location for the PV guest
-    # this is a string pointing to an NFS, HTTP or FTP install source 
-    def get_install_location(self):
-        return self._location
-    def set_install_location(self, val):
-        if not (val.startswith("http://") or val.startswith("ftp://") or
-                val.startswith("nfs:")):
-            raise ValueError, "Install location must be an NFS, HTTP or FTP install source"
-        self._location = val
-    location = property(get_install_location, set_install_location)
+    def start_install(self, connectConsole = False):
+        if not self.cdrom:
+            raise RuntimeError, "A CD must be specified to boot from"
+        XenGuest.XenGuest.validateParms(self)
+        
+        conn = libvirt.openReadOnly(None)
+        if conn == None:
+            raise RuntimeError, "Unable to connect to hypervisor, aborting installation!"
+        try:
+            if conn.lookupByName(self.name) is not None:
+                raise RuntimeError, "Domain named %s already exists!" %(self.name,)
+        except libvirt.libvirtError:
+            pass
+
+        self._createDevices()
+        cxml = self._get_config_xml()
+        print cxml
+        self.domain = conn.createLinux(cxml, 0)
+        if self.domain is None:
+            raise RuntimeError, "Unable to create domain for guest, aborting installation!"
+
+        time.sleep(5)
+        # FIXME: if the domain doesn't exist now, it almost certainly crashed.
+        # it'd be nice to know that for certain...
+        try:
+            d = conn.lookupByID(self.domain.ID())
+        except libvirt.libvirtError:
+            raise RuntimeError, "It appears that your installation has crashed.  You should be able to find more information in the xen logs"
 
 
-    # extra arguments to pass to the guest installer
-    def get_extra_args(self):
-        return self._extraargs
-    def set_extra_args(self, val):
-        self._extraargs = val
-    extraargs = property(get_extra_args, set_extra_args)
-
-
-    def start_install(self):
-        pass
+        cf = "/etc/xen/%s" %(self.name,)
+        f = open(cf, "w+")
+        f.write(self._get_config_xen())
+        f.close()
+        return 
