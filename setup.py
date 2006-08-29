2eee974553a012d832c21fca0e66f4a287a32b8f
Jeremy Katz <katzj@redhat.com>
2006-08-29 18:16:32 -0400

    bump version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 pkgs = ['xeninst']
 setup(name='xeninst',
-      version='0.90.1',
+      version='0.91.0',
       description='Xen guest installation',
       author='Jeremy Katz',
       author_email='katzj@redhat.com',
