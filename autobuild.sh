2b3225a3ccbda5cfcabf34b551bd1dbfffca35da
Daniel P. Berrange <berrange@redhat.com>
2007-01-29 11:49:12 -0500

    Added autobuild control file for build/test automation purposes

---
+++ autobuild.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+rm -rf build dist python-virtinst.spec
+
+python setup.py build
+python setup.py test
+python setup.py install --prefix=$AUTOBUILD_INSTALL_ROOT
+
+VERSION=`python setup.py --version`
+cat python-virtinst.spec.in | sed -e "s/::VERSION::/$VERSION/" > python-virtinst.spec
+python setup.py sdist
+
+if [ -f /usr/bin/rpmbuild ]; then
+  if [ -n "$AUTOBUILD_COUNTER" ]; then
+    EXTRA_RELEASE=".auto$AUTOBUILD_COUNTER"
+  else
+    NOW=`date +"%s"`
+    EXTRA_RELEASE=".$USER$NOW"
+  fi
+  rpmbuild --define "extra_release $EXTRA_RELEASE" \
+           --define "_sourcedir `pwd`/dist" \
+           -ba --clean python-virtinst.spec
+fi
