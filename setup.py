7363fe5db2e756157c91002e30ad08d1b2deef06
Daniel P. Berrange <berrange@redhat.com>
2007-07-18 22:02:24 -0400

    Fixed typo in __init__.py name

--- setup.py
+++ setup.py
@@ -82,7 +82,7 @@ class install_lib(_install_lib):
 
     def run(self):
         tmplocale = locale.replace("/", "\/") # Escape path to call sed
-        cmd = "sed -e \"s/::LOCALEDIR::/%s/\" < virtinst/__init.py > " % tmplocale +\
+        cmd = "sed -e \"s/::LOCALEDIR::/%s/\" < virtinst/__init__.py > " % tmplocale +\
               "%s/virtinst/__init__.py" % builddir
         os.system(cmd)
         _install_lib.run(self)
