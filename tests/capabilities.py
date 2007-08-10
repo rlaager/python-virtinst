fc89e1cbea75ae1d628ff6c5092340fc2009eb13
Daniel P. Berrange <berrange@redhat.com>
2007-08-10 07:55:39 -0400

    Fixed up test for changes in capabilities API

--- tests/capabilities.py
+++ tests/capabilities.py
@@ -8,47 +8,49 @@ class TestCapabilities(unittest.TestCase):
         self.assertEqual(arch,            guest.arch)
         self.assertEqual(os_type,         guest.os_type)
         self.assertEqual(hypervisor_type, guest.hypervisor_type)
-        self.assertEqual(features,        guest.features)
+        for n in features:
+            self.assertEqual(features[n],        guest.features[n])
 
     def _testCapabilities(self, path, (host_arch, host_features), guests):
         caps = capabilities.parse(file(os.path.join("tests", path)).read())
 
         self.assertEqual(host_arch,     caps.host.arch)
-        self.assertEqual(host_features, caps.host.features)
+        for n in host_features:
+            self.assertEqual(host_features[n], caps.host.features[n])
 
         map(self._compareGuest, guests, caps.guests)
 
     def testCapabilities1(self):
-        host = ( 'x86_64', capabilities.FEATURE_VMX )
+        host = ( 'x86_64', {'vmx': capabilities.FEATURE_ON} )
 
         guests = [
-            ( 'x86_64', 'xen', 'xen', 0 ),
-            ( 'i686',   'xen', 'xen', capabilities.FEATURE_PAE ),
-            ( 'i686',   'hvm', 'xen', capabilities.FEATURE_PAE|capabilities.FEATURE_NONPAE ),
-            ( 'x86_64', 'hvm', 'xen', 0 )
+            ( 'x86_64', 'xen', 'xen', {} ),
+            ( 'i686',   'xen', 'xen', { 'pae': capabilities.FEATURE_ON } ),
+            ( 'i686',   'hvm', 'xen', { 'pae': capabilities.FEATURE_ON|capabilities.FEATURE_OFF } ),
+            ( 'x86_64', 'hvm', 'xen', {} )
         ]
 
         self._testCapabilities("capabilities-xen.xml", host, guests)
 
     def testCapabilities2(self):
-        host = ( 'x86_64', 0 )
+        host = ( 'x86_64', {} )
 
         guests = [
-            ( 'x86_64', 'hvm', 'qemu', 0 ),
-            ( 'i686',   'hvm', 'qemu', 0 ),
-            ( 'mips',   'hvm', 'qemu', 0 ),
-            ( 'mipsel', 'hvm', 'qemu', 0 ),
-            ( 'sparc',  'hvm', 'qemu', 0 ),
-            ( 'ppc',    'hvm', 'qemu', 0 ),
+            ( 'x86_64', 'hvm', 'qemu', {} ),
+            ( 'i686',   'hvm', 'qemu', {} ),
+            ( 'mips',   'hvm', 'qemu', {} ),
+            ( 'mipsel', 'hvm', 'qemu', {} ),
+            ( 'sparc',  'hvm', 'qemu', {} ),
+            ( 'ppc',    'hvm', 'qemu', {} ),
         ]
 
         self._testCapabilities("capabilities-qemu.xml", host, guests)
 
     def testCapabilities3(self):
-        host = ( 'i686', capabilities.FEATURE_PAE|capabilities.FEATURE_NONPAE )
+        host = ( 'i686', { 'pae': capabilities.FEATURE_ON|capabilities.FEATURE_OFF } )
 
         guests = [
-            ( 'i686', 'linux', 'test', capabilities.FEATURE_PAE|capabilities.FEATURE_NONPAE ),
+            ( 'i686', 'linux', 'test', { 'pae': capabilities.FEATURE_ON|capabilities.FEATURE_OFF } ),
         ]
 
         self._testCapabilities("capabilities-test.xml", host, guests)
