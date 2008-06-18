d4cc7a6649cc35cdcf584826d1aac95fe11ec00c
Cole Robinson <crobinso@redhat.com>
2008-06-18 12:18:20 -0400

    Explictly import translation function for command line utilities

--- virtinst/cli.py
+++ virtinst/cli.py
@@ -27,7 +27,8 @@ from optparse import OptionValueError, OptionParser
 
 import libvirt
 import util
-import Guest, CapabilitiesParser
+from virtinst import Guest, CapabilitiesParser
+from virtinst import _virtinst as _
 
 MIN_RAM = 64
 force = False
