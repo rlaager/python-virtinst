5cc7f1fa12c8a4d6c078ad259822e54a7b62e658
Cole Robinson <crobinso@redhat.com>
2008-12-16 14:10:14 -0500

    Break out xmlconfig guest building helpers for use in other test modules.

--- tests/xmlconfig.py
+++ tests/xmlconfig.py
@@ -22,6 +22,35 @@ import urlgrabber.progress as progress
 import virtinst
 import tests
 
+def get_basic_paravirt_guest():
+    conn = libvirt.open("test:///default")
+    g = virtinst.ParaVirtGuest(connection=conn, type="xen")
+    g.name = "TestGuest"
+    g.memory = int(200)
+    g.maxmemory = int(400)
+    g.uuid = "12345678-1234-1234-1234-123456789012"
+    g.boot = ["/boot/vmlinuz","/boot/initrd"]
+    g.graphics = (True, "vnc", None, "ja")
+    g.vcpus = 5
+    return g
+
+def get_basic_fullyvirt_guest():
+    conn = libvirt.open("test:///default")
+    g = virtinst.FullVirtGuest(connection=conn, type="xen",
+                               emulator="/usr/lib/xen/bin/qemu-dm",
+                               arch="i686")
+    g.name = "TestGuest"
+    g.memory = int(200)
+    g.maxmemory = int(400)
+    g.uuid = "12345678-1234-1234-1234-123456789012"
+    g.cdrom = "/dev/loop0"
+    g.set_os_type("other")
+    g.set_os_variant("generic")
+    g.graphics = (True, "sdl")
+    g.features['pae'] = 0
+    g.vcpus = 5
+    return g
+
 class TestXMLConfig(unittest.TestCase):
 
     def _compare(self, xenguest, filebase, install):
@@ -33,72 +62,44 @@ class TestXMLConfig(unittest.TestCase):
         finally:
             xenguest.installer.cleanup()
 
-    def _get_basic_paravirt_guest(self):
-        conn = libvirt.openReadOnly("test:///default")
-        g = virtinst.ParaVirtGuest(connection=conn, type="xen")
-        g.name = "TestGuest"
-        g.memory = int(200)
-        g.maxmemory = int(400)
-        g.uuid = "12345678-1234-1234-1234-123456789012"
-        g.boot = ["/boot/vmlinuz","/boot/initrd"]
-        g.graphics = (True, "vnc", None, "ja")
-        g.vcpus = 5
-        return g
-
-    def _get_basic_fullyvirt_guest(self):
-        conn = libvirt.openReadOnly("test:///default")
-        g = virtinst.FullVirtGuest(connection=conn, type="xen",
-                                   emulator="/usr/lib/xen/bin/qemu-dm",
-                                   arch="i686")
-        g.name = "TestGuest"
-        g.memory = int(200)
-        g.maxmemory = int(400)
-        g.uuid = "12345678-1234-1234-1234-123456789012"
-        g.cdrom = "/dev/loop0"
-        g.set_os_type("other")
-        g.set_os_variant("generic")
-        g.graphics = (True, "sdl")
-        g.features['pae'] = 0
-        g.vcpus = 5
-        return g
 
     def testBootParavirtDiskFile(self):
-        g = self._get_basic_paravirt_guest()
+        g = get_basic_paravirt_guest()
         g.disks.append(virtinst.VirtualDisk("/tmp/test.img", type=virtinst.VirtualDisk.TYPE_FILE,size=5))
         self._compare(g, "boot-paravirt-disk-file", False)
 
     def testBootParavirtDiskBlock(self):
-        g = self._get_basic_paravirt_guest()
+        g = get_basic_paravirt_guest()
         g.disks.append(virtinst.VirtualDisk("/dev/loop0", type=virtinst.VirtualDisk.TYPE_BLOCK))
         self._compare(g, "boot-paravirt-disk-block", False)
 
     def testBootParavirtDiskDrvPhy(self):
-        g = self._get_basic_paravirt_guest()
+        g = get_basic_paravirt_guest()
         g.disks.append(virtinst.VirtualDisk("/dev/loop0", type=virtinst.VirtualDisk.TYPE_BLOCK, \
                                        driverName = virtinst.VirtualDisk.DRIVER_PHY))
         self._compare(g, "boot-paravirt-disk-drv-phy", False)
 
     def testBootParavirtDiskDrvFile(self):
-        g = self._get_basic_paravirt_guest()
+        g = get_basic_paravirt_guest()
         g.disks.append(virtinst.VirtualDisk("/tmp/test.img", type=virtinst.VirtualDisk.TYPE_FILE, \
                                        driverName = virtinst.VirtualDisk.DRIVER_FILE,size=5))
         self._compare(g, "boot-paravirt-disk-drv-file", False)
 
     def testBootParavirtDiskDrvTap(self):
-        g = self._get_basic_paravirt_guest()
+        g = get_basic_paravirt_guest()
         g.disks.append(virtinst.VirtualDisk("/tmp/test.img", type=virtinst.VirtualDisk.TYPE_FILE, \
                                        driverName = virtinst.VirtualDisk.DRIVER_TAP,size=5))
         self._compare(g, "boot-paravirt-disk-drv-tap", False)
 
     def testBootParavirtDiskDrvTapQCow(self):
-        g = self._get_basic_paravirt_guest()
+        g = get_basic_paravirt_guest()
         g.disks.append(virtinst.VirtualDisk("/tmp/test.img", type=virtinst.VirtualDisk.TYPE_FILE, \
                                        driverName = virtinst.VirtualDisk.DRIVER_TAP, \
                                        driverType = virtinst.VirtualDisk.DRIVER_TAP_QCOW,size=5))
         self._compare(g, "boot-paravirt-disk-drv-tap-qcow", False)
 
     def testBootParavirtManyDisks(self):
-        g = self._get_basic_paravirt_guest()
+        g = get_basic_paravirt_guest()
         g.disks.append(virtinst.VirtualDisk("/tmp/test1.img", type=virtinst.VirtualDisk.TYPE_FILE,size=5))
         g.disks.append(virtinst.VirtualDisk("/tmp/test2.img", type=virtinst.VirtualDisk.TYPE_FILE, \
                                        driverName = virtinst.VirtualDisk.DRIVER_TAP, \
@@ -107,12 +108,12 @@ class TestXMLConfig(unittest.TestCase):
         self._compare(g, "boot-paravirt-many-disks", False)
 
     def testBootFullyvirtDiskFile(self):
-        g = self._get_basic_fullyvirt_guest()
+        g = get_basic_fullyvirt_guest()
         g.disks.append(virtinst.VirtualDisk("/tmp/test.img", type=virtinst.VirtualDisk.TYPE_FILE,size=5))
         self._compare(g, "boot-fullyvirt-disk-file", False)
 
     def testBootFullyvirtDiskBlock(self):
-        g = self._get_basic_fullyvirt_guest()
+        g = get_basic_fullyvirt_guest()
         g.disks.append(virtinst.VirtualDisk("/dev/loop0", type=virtinst.VirtualDisk.TYPE_BLOCK))
         self._compare(g, "boot-fullyvirt-disk-block", False)
 
@@ -120,42 +121,42 @@ class TestXMLConfig(unittest.TestCase):
 
 
     def testInstallParavirtDiskFile(self):
-        g = self._get_basic_paravirt_guest()
+        g = get_basic_paravirt_guest()
         g.disks.append(virtinst.VirtualDisk("/tmp/test.img", type=virtinst.VirtualDisk.TYPE_FILE,size=5))
         self._compare(g, "install-paravirt-disk-file", True)
 
     def testInstallParavirtDiskBlock(self):
-        g = self._get_basic_paravirt_guest()
+        g = get_basic_paravirt_guest()
         g.disks.append(virtinst.VirtualDisk("/dev/loop0", type=virtinst.VirtualDisk.TYPE_BLOCK))
         self._compare(g, "install-paravirt-disk-block", True)
 
     def testInstallParavirtDiskDrvPhy(self):
-        g = self._get_basic_paravirt_guest()
+        g = get_basic_paravirt_guest()
         g.disks.append(virtinst.VirtualDisk("/dev/loop0", type=virtinst.VirtualDisk.TYPE_BLOCK, \
                                        driverName = virtinst.VirtualDisk.DRIVER_PHY))
         self._compare(g, "install-paravirt-disk-drv-phy", True)
 
     def testInstallParavirtDiskDrvFile(self):
-        g = self._get_basic_paravirt_guest()
+        g = get_basic_paravirt_guest()
         g.disks.append(virtinst.VirtualDisk("/tmp/test.img", type=virtinst.VirtualDisk.TYPE_FILE, \
                                        driverName = virtinst.VirtualDisk.DRIVER_FILE,size=5))
         self._compare(g, "install-paravirt-disk-drv-file", True)
 
     def testInstallParavirtDiskDrvTap(self):
-        g = self._get_basic_paravirt_guest()
+        g = get_basic_paravirt_guest()
         g.disks.append(virtinst.VirtualDisk("/tmp/test.img", type=virtinst.VirtualDisk.TYPE_FILE, \
                                        driverName = virtinst.VirtualDisk.DRIVER_TAP,size=5))
         self._compare(g, "install-paravirt-disk-drv-tap", True)
 
     def testInstallParavirtDiskDrvTapQCow(self):
-        g = self._get_basic_paravirt_guest()
+        g = get_basic_paravirt_guest()
         g.disks.append(virtinst.VirtualDisk("/tmp/test.img", type=virtinst.VirtualDisk.TYPE_FILE, \
                                        driverName = virtinst.VirtualDisk.DRIVER_TAP, \
                                        driverType = virtinst.VirtualDisk.DRIVER_TAP_QCOW,size=5))
         self._compare(g, "install-paravirt-disk-drv-tap-qcow", True)
 
     def testInstallParavirtManyDisks(self):
-        g = self._get_basic_paravirt_guest()
+        g = get_basic_paravirt_guest()
         g.disks.append(virtinst.VirtualDisk("/tmp/test1.img", type=virtinst.VirtualDisk.TYPE_FILE,size=5))
         g.disks.append(virtinst.VirtualDisk("/tmp/test2.img", type=virtinst.VirtualDisk.TYPE_FILE, \
                                        driverName = virtinst.VirtualDisk.DRIVER_TAP, \
@@ -164,31 +165,30 @@ class TestXMLConfig(unittest.TestCase):
         self._compare(g, "install-paravirt-many-disks", True)
 
     def testInstallFullyvirtDiskFile(self):
-        g = self._get_basic_fullyvirt_guest()
+        g = get_basic_fullyvirt_guest()
         g.disks.append(virtinst.VirtualDisk("/tmp/test.img", type=virtinst.VirtualDisk.TYPE_FILE,size=5))
         self._compare(g, "install-fullyvirt-disk-file", True)
 
     def testInstallFullyvirtDiskBlock(self):
-        g = self._get_basic_fullyvirt_guest()
+        g = get_basic_fullyvirt_guest()
         g.disks.append(virtinst.VirtualDisk("/dev/loop0", type=virtinst.VirtualDisk.TYPE_BLOCK))
         self._compare(g, "install-fullyvirt-disk-block", True)
 
     def testInstallFVPXE(self):
-        g = self._get_basic_fullyvirt_guest()
+        g = get_basic_fullyvirt_guest()
         g.installer = virtinst.PXEInstaller(type="xen", os_type="hvm", conn=g.conn)
         self._compare(g, "install-fullyvirt-pxe", True)
 
     def testInstallFVLiveCD(self):
-        g = self._get_basic_fullyvirt_guest()
+        g = get_basic_fullyvirt_guest()
         g.installer = virtinst.LiveCDInstaller(type="xen", os_type="hvm", conn=g.conn, location="/dev/loop0")
         self._compare(g, "install-fullyvirt-livecd", False)
 
     def testXMLEscaping(self):
-        g = self._get_basic_fullyvirt_guest()
+        g = get_basic_fullyvirt_guest()
         g.disks.append(virtinst.VirtualDisk("/tmp/ISO&'&s", type=virtinst.VirtualDisk.TYPE_FILE, size=5))
         self._compare(g, "misc-xml-escaping", True)
 
 
 if __name__ == "__main__":
     unittest.main()
-    
