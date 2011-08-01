rlaager/python-virtinst

commit c81dd1c081a55fa13a54494094f801682191e948

setup.py: Only build _config.py if it has changed

setup.py
@@ -312,11 +312,16 @@ class mybuild(build):
             "VERSION" : VERSION,
             "RHEL6DEFAULTS" : self.rhel6defaults,
         }
+
         config_data = config_template % config_opts
         print "Version              : %s" % VERSION
         print "RHEL6 defaults       : %s" % bool(self.rhel6defaults)
 
         for f in config_files:
+            origconfig = file(f).read()
+            if origconfig == config_data:
+                continue
+
             print "Generating %s" % f
             fd = open(f, "w")
             fd.write(config_data)

commit 7a84d5d4efce6bb90a8697c5484c90cebb165a8a

setup.py: Build _config.py if missing
Commit c81dd1c081a55fa13a54494094f801682191e948 introduced regression.
When _config.py does not exists, we end up trying to read non-existing
file and hence exception.

(crobinso: adapted to use path.exists rather than catch exception)

AUTHORS
@@ -51,6 +51,7 @@ Patches also received from
     Roopa Prabhu <roprabhu-at-cisco-dot-com>
     Andres Rodriguez <andreserl-at-ubuntu-dot-com>
     Hajime Taira <htaira-at-redhat-dot-com>
+    Michal Prívozník <mprivozn-at-redhat-dot-com>
 
     ...send patches and get your name here...
 

setup.py
@@ -330,9 +330,10 @@ class mybuild(build):
         print "RHEL6 defaults       : %s" % bool(self.rhel6defaults)
 
         for f in config_files:
-            origconfig = file(f).read()
-            if origconfig == config_data:
-                continue
+            if os.path.exists(f):
+                origconfig = file(f).read()
+                if origconfig == config_data:
+                    continue
 
             print "Generating %s" % f
             fd = open(f, "w")